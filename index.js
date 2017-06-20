2746a59e758ed117a355565e57ab42f7c351c9d9
Boris <boris.pozzo@gmail.com>
2017-06-20 23:30:24 +0200

    restart

--- index.js
+++ index.js
@@ -1,74 +1,33 @@
 'use strict'
 
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-const token = process.env.FB_VERIFY_TOKEN
-const access = process.env.FB_ACCESS_TOKEN
-
 app.set('port', (process.env.PORT || 5000))
 
+// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
 app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
-});
+    res.send('Hello world, I am a chat bot')
+})
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === token) {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-});
-
-//POST data
-app.post('/webhook', function (req, res) {
-  let messaging_events =req.body.entry[0].messaging
-  for (let i = 0; i < messaging_events.length; i++){
-      let event = req.body.entry[0].messaging[i]
-  }
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('No sir')
 })
-  
-
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
-});
-  
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
