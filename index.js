1693d8ff6b50b9ccb9cbe3a2b3fc964f2ce17f3e
Boris <boris.pozzo@gmail.com>
2017-06-20 23:16:21 +0200

    make it work

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const token = process.env.FB_PAGE_ACCESS_TOKEN;
+
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
 
 app.set('port', (process.env.PORT || 5000))
 
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
 
 // Index route
@@ -21,13 +20,21 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === token) {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
 });
 
 //POST data
+app.post('/webhook', function (req, res) {
+  let messaging_events =req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++){
+      let event = req.body.entry[0].messaging[i]
+  }
+})
+  
+
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
@@ -61,79 +68,6 @@ app.post('/webhook', function (req, res) {
 function receivedMessage(event) {
   // Putting a stub for now, we'll expand it in the following steps
   console.log("Message data: ", event.message);
-};
-
-
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-};
-
-function sendGenericMessage(recipientId, messageText) {
-  // To be expanded in later sections
-};
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-};
-
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
 }
 
 // Spin up the server
